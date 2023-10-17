08aac0445d34335f3c1c4acba7b823550a3a3102
Chu-Chu Idigo <cidigo97@gmail.com>
2023-10-17 12:47:14 -0400

    able to pull league info and find associated clubs

--- foreign_analysis/console.py
+++ foreign_analysis/console.py
@@ -1,6 +1,7 @@
-from etl.extract_data import get_champions_league_clubs, get_leagues_from_clubs
+from etl.extract_data import get_league_clubs, get_leagues_from_clubs, get_clubs_leagues
 
-champions_league_clubs = get_champions_league_clubs()
+# league_id = input("Enter the league_id of the league you want to check")
+# champions_league_clubs = get_champions_league_clubs()
 
 
 
--- foreign_analysis/etl/extract_data.py
+++ foreign_analysis/etl/extract_data.py
@@ -8,37 +8,41 @@ api_headers = {
         }
 
 
-# The purpose of this portion here is to get all the teams that are in the champions league. Then from there we will look at all
-# the leagues that sent a team to the champions league(we are starting from here as it's Europe's most premiere competition)
-# while looking at how the teams in the champions league are  displayed we see they are displayed in a dictionary of a list of 
-# dictionaries with each dictionary being a team and containing the following key value pairs ("id": "teamid",  
-# "name": "team_name", "image": "link_to_team_image")
-def get_champions_league_clubs():
+# The purpose of this portion here is to get all the teams that are in the league. Then from there we will look at all
+# the clubs in that league while looking at how the teams in the league are displayed we see they are displayed in a
+# dictionary of a list of dictionaries with each dictionary being a team and containing the following 
+# key value pairs ("id": "teamid", "name": "team_name", "image": "link_to_team_image")
+def get_league_clubs():
 
+    # league_id = str(input("Enter the league_id of the league to explore: "))
     url = "https://transfermarket.p.rapidapi.com/clubs/list-by-competition"
-    querystring = {"id":input(),"domain":api_domain}
+    querystring = {"id": "cl","domain":api_domain}
     headers = api_headers
     response = requests.get(url, headers=headers, params=querystring)
-    champions_league_clubs_json = response.json()["clubs"]
-    breakpoint()
-    return champions_league_clubs_json
+    league_clubs_json = response.json()["clubs"]
+    # breakpoint()
+    return league_clubs_json
+
 
 # Using the data retrieved from get_champions_league_teams or another function that gets a list of clubs. We will call 
 # some information about each team in the list and get the domestic leagues that teams in the list belong to
-# along with an updated version of our original list with their domestic leagues now included
-def get_leagues_from_clubs(clubs_dict):
+# along with an updated version of our original list with their domestic leagues now included    
+def get_leagues_from_clubs():
 
-    clubs_json = clubs_dict
+    clubs_json = get_league_clubs()
 
     # loop through the list of dictionary of clubs. With each loop hit the api to get the domestic league each team in the
-    # the list belongs to and add them to the 'clubs_json' list of dictionaries as "league": "league identifier" then from there add 
+    # list belongs to and add them to the 'clubs_json' list of dictionaries as "league": "league identifier" then from there add 
     # the league to a seperate list we'll call clubs_leagues and apply a set to that list to get all the leagues involved in the
     # competition without any duplicates or repeats and call the cleaned one clubs_leagues_cleaned
+    clubs_leagues_info = {}
     clubs_leagues = []
     n = 0
+    # breakpoint()
 
     for club in clubs_json:
         url = "https://transfermarket.p.rapidapi.com/clubs/get-header-info"
+        # breakpoint()
 
         querystring = {"id":club["id"],"domain":api_domain}
 
@@ -48,10 +52,37 @@ def get_leagues_from_clubs(clubs_dict):
     
         # breakpoint()
         time.sleep(1)
-        clubs_leagues.append(response.json()["club"]["leagueID"])
         clubs_json[n].update({"leagueID": response.json()["club"]["leagueID"], "leagueName": response.json()["club"]["leagueName"]})
+        if not clubs_leagues_info:
+            clubs_leagues_info[response.json()["club"]["leagueID"]] = response.json()["club"]["leagueName"]
+            clubs_leagues.append(response.json()["club"]["leagueID"])
+        elif response.json()["club"]["leagueID"] not in clubs_leagues:
+            clubs_leagues_info[response.json()["club"]["leagueID"]] = response.json()["club"]["leagueName"]
+            clubs_leagues.append(response.json()["club"]["leagueID"])
         n += 1
         
-    clubs_leagues_cleaned = list(set(clubs_leagues))
     # breakpoint()
-    return (clubs_json, clubs_leagues_cleaned)
+    return (clubs_json, clubs_leagues_info)
+
+
+# The purpose of this function is to get the clubs that are located in all the different leagues that are passed in to 
+# it. So if 5 separate leagues are fed into it, it'll produce all the teams for those 5 leagues
+def get_clubs_leagues():
+    
+    leagues_from_clubs = get_leagues_from_clubs()
+    # breakpoint()
+    league_ids = list(leagues_from_clubs[1].keys())
+    leagues_info = []
+    # breakpoint()
+    for league_id in league_ids:
+        # breakpoint()
+        url = "https://transfermarket.p.rapidapi.com/clubs/list-by-competition"
+        querystring = {"id": league_id,"domain":api_domain}
+        headers = api_headers
+        response = requests.get(url, headers=headers, params=querystring)
+        league_clubs_json = response.json()["clubs"]
+        leagues_info.append({"leauge_id":league_id, "league_name": leagues_from_clubs[1][league_id], 
+                             "league_clubs":league_clubs_json})    
+
+    # breakpoint()
+    return
